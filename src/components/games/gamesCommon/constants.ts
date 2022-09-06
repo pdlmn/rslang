d45ba422301498715c8f72cd1086dbab9e1ae4f3
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-09-06 07:00:06 +0500

    fix: sound paths

--- src/components/games/gamesCommon/constants.ts
+++ src/components/games/gamesCommon/constants.ts
@@ -1,7 +1,7 @@
-export enum SoundUrl {
-  RightAnswer = '/assets/audio/rightAnswer.wav',
-  WrongAnswer = '/assets/audio/wrongAnswer.wav',
-  Finish = '/assets/audio/finish.wav',
-  MultiplerGain = '/assets/audio/multiplerPlus.wav',
-  MultiplerLoss = '/assets/audio/wrongAnswer2.wav',
-}
+export const SoundUrl = {
+  RightAnswer: `${process.env.PUBLIC_URL}/assets/audio/rightAnswer.wav`,
+  WrongAnswer: `${process.env.PUBLIC_URL}/assets/audio/wrongAnswer.wav`,
+  Finish: `${process.env.PUBLIC_URL}/assets/audio/finish.wav`,
+  MultiplerGain: `${process.env.PUBLIC_URL}/assets/audio/multiplerPlus.wav`,
+  MultiplerLoss: `${process.env.PUBLIC_URL}/assets/audio/wrongAnswer2.wav`,
+};
